1c6c016cbb64aae6be15859d768b18a0ed2b3525
Rimanshu Mangal <35160121+rimanshu@users.noreply.github.com>
2018-10-12 05:20:55 -0500

    Update matpltlib.py

--- matpltlib.py
+++ matpltlib.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # plt.plot([1,2,3],[9,7,8]) or 
 
@@ -7,6 +8,11 @@ y = ['Banking','pharma']
 
 x2 = [1,2,3]
 y2 = [10,14,12]
+
+# Load in data
+tips = pd.read_csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/tips.csv")
+
+#plt.bar(x="total_bill", y="tip" , data = tips) - to plot from dataset
 
 plt.pie(x,y)
 #plt.scatter(x,y,label ='scatter', color ='b', marker='*', s=200) #s for size
